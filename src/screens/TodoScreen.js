77bc365863b2e62d4b403eab8a876f9ad0f19535
MakkaAkhrieva <makka.akhrieva00@mail.ru>
2023-03-29 17:52:02 +0300

    responsive styles according to dimensions

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
 import { THEME } from "../theme";
 import { FontAwesome, AntDesign } from "@expo/vector-icons";
 import { AppCard } from "../components/ui/AppCard";
@@ -62,7 +62,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   button: {
-    width: "40%",
+    width: Dimensions.get("window").width / 3,
   },
   title: {
     fontSize: 20,
