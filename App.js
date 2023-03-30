22396982dd4b55dbf591f11537ae85082674cb92
MakkaAkhrieva <makka.akhrieva00@mail.ru>
2023-03-30 15:19:56 +0300

    Screen Orientation Adaptation

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { MainScreen } from "./src/screens/MainScreen";
 import { TodoScreen } from "./src/screens/TodoScreen";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
+import { THEME } from "./src/theme";
 
 async function loadApplication() {
   await Font.loadAsync({
@@ -113,7 +114,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 30,
+    paddingHorizontal: THEME.PADDING_HORIZONTAL,
     paddingVertical: 20,
   },
 });
